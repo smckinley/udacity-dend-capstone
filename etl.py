efc8ac58eda1f71a417c8277cc4c3861aaaf8f9f
Sky McKinley <sky.mckinley@gmail.com>
2020-05-02 15:04:13 -0700

    Configuration processing and function arguments.

--- etl.py
+++ etl.py
@@ -2,16 +2,16 @@ import pandas as pd
 import configparser
 from pyspark.sql import SparkSession
 
-def extract_flight_data():
+def extract_flight_data(input_location, output_location):
 	return 1;
 
-def extract_airport_data():
+def extract_airport_data(input_location, output_location):
 	return 1;
 
 def process_flight_dims():
 	return 1;
 
-def process_airport_dis():
+def process_airport_dims():
 	return 1;
 
 def process_flight_facts():
@@ -20,11 +20,20 @@ def process_flight_facts():
 def main():
 	print("Starting ETL")
 
+	# Process ETL configurations.
+	etl_config = configparser.ConfigParser()
+	etl_config.read('etl.cfg')
+
+	flight_data_location = etl_config['INPUT']['DATA_LOCATION_I94']
+	airport_data_location = etl_config['INPUT']['DATA_LOCATION_AIRPORTS']
+
+	output_data_location = etl_config['OUTPUT']['DATA_LOCATION_OUTPUT']
+
 	# Read in flight information.
-	extract_flight_data()
+	extract_flight_data(flight_data_location, output_data_location)
 
 	# Read in airport information.
-	extract_airport_data()
+	extract_airport_data(airport_data_location, output_data_location)
 
 	# Process flight dimensions.
 	process_flight_dims()
