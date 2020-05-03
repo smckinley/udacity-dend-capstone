dcb66eb951c0bd0c3c31b82fd8b61e3ba59ba7e3
Sky McKinley <sky.mckinley@gmail.com>
2020-05-02 17:19:08 -0700

    Clear extract file locations before extracting data. Tidied up file paths.

--- etl.py
+++ etl.py
@@ -1,28 +1,32 @@
 import pandas as pd
 import configparser
-from os import listdir
+import os
 from pyspark.sql import SparkSession
 
 def extract_flight_data(input_location, output_location):
-	print("Reading from " + input_location)
-	print("Writing to " + output_location)
+	if os.path.exists(output_location):
+		for f in os.listdir(output_location):
+			os.remove(output_location + "/" + f)
 
 	spark = SparkSession.builder\
 		.config("spark.jars.packages","saurfang:spark-sas7bdat:2.0.0-s_2.11")\
 		.enableHiveSupport().getOrCreate()
 
-	for f in listdir(input_location):
+	for f in os.listdir(input_location):
 		df_spark = spark.read.format('com.github.saurfang.sas.spark')\
 			.option('header', 'true')\
 			.load(input_location + "/" + f)
 
-		df_spark.write.mode('append').parquet(output_location + "sas_data")
+		df_spark.write.mode('append').parquet(output_location)
+
+		# While testing just load the first file.
+		break
 
 def extract_airport_data(input_location, output_location):
 	print("Reading from " + input_location)
 	print("Writing to " + output_location)
 
-def tranform_flight_data():
+def transform_flight_data():
 	return 1
 
 def transform_airport_data():
@@ -53,7 +57,9 @@ def main():
 	complete_location = etl_config['OUTPUT']['DATA_LOCATION_COMPLETE']
 
 	# Read in flight information.
-	extract_flight_data(flight_data_location, extract_location)
+	flight_data_extract_dir = "sas_data"
+	extract_flight_data(flight_data_location, extract_location + "/" \
+		+ flight_data_extract_dir)
 
 	# Read in airport information.
 	extract_airport_data(airport_data_location, extract_location)
@@ -62,7 +68,7 @@ def main():
 	transform_flight_data()
 
 	# Transform airport data.
-	tranform_airport_data()	
+	transform_airport_data()	
 
 	# Load flight dimensions.
 	load_flight_dims()
